4ea6c11774783f92dcd8a068ef982a3b861db57f
Jin <lochjin@gmail.com>
2023-09-13 17:40:06 +0800

    Optimize miner for qng

--- miner/miner_qng.go
+++ miner/miner_qng.go
@@ -3,8 +3,10 @@ package miner
 import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/core/txpool"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/event"
+	"math/big"
 	"time"
 )
 
@@ -23,4 +25,14 @@ type IMiner interface {
 	SetGasCeil(ceil uint64)
 	SubscribePendingLogs(ch chan<- []*types.Log) event.Subscription
 	BuildPayload(args *BuildPayloadArgs) (*Payload, error)
+}
+
+type TransactionsByPriceAndNonce interface {
+	Peek() *txpool.LazyTransaction
+	Shift()
+	Pop()
+}
+
+func NewTransactionsByPriceAndNonce(signer types.Signer, txs map[common.Address][]*txpool.LazyTransaction, baseFee *big.Int) TransactionsByPriceAndNonce {
+	return newTransactionsByPriceAndNonce(signer,txs,baseFee)
 }
